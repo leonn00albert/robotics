f0c0b285d8dfa4d7a876f2a9758d19225f9b628d
leon <leon00albert@gmail.com>
2024-01-24 20:19:00 +0100

    dev: add readme and motor test

---
+++ tests/test_motor.py
@@ -0,0 +1,21 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+
+import time
+import atexit
+
+mh = Raspi_MotorHAT(addr=0x40)
+lm = mh.getMotor(1)
+rm = mh.getMotor(2)
+
+def turn_off_motors():
+  lm.run(Raspi_MotorHAT.RELEASE)
+  rm.run(Raspi_MotorHAT.RELEASE)
+
+atexit.register(turn_off_motors)
+
+lm.setSpeed(255)
+rm.setSpeed(255)
+
+lm.run(Raspi_MotorHAT.FORWARD)
+rm.run(Raspi_MotorHAT.FORWARD)
+time.sleep(1)

628dd72dc910ba33404a8da8388e0fc64478e3d8
leon <leon00albert@gmail.com>
2024-02-14 22:41:41 +0100

    test: add distance sensor test

--- src/test_distance_sensors.py
+++ src/test_distance_sensors.py
@@ -4,3 +4,8 @@ from gpiozero import DistanceSensor
 print('Prepare GPIO pins')
 sensor_l = DistanceSensor(echo=17, trigger= 27, queue_len=2)
 sensor_r = DistanceSensor(echo=5, trigger= 6, queue_len=2)
+
+while True:
+    print("left: {l:.2f}, Right{r:.2f}".format(l=sensor_l.distance * 100, r=sensor_r.distance * 100,))
+    time.sleep(0.1)
+
